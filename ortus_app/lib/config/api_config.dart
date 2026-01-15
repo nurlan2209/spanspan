96385a3a5c44b17e84abaa750a132178abbfa213
nurlan2209 <nkuanyshuly2020@gmail.com>
2026-01-15 07:07:23 +0500

    new2

--- ortus_app/lib/config/api_config.dart
+++ ortus_app/lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://spanspan-production.up.railway.app';
+  static const String baseUrl = 'https://spanspan-production.up.railway.app/api';
 
   static const String authUrl = '$baseUrl/auth';
   static const String usersUrl = '$baseUrl/users';
